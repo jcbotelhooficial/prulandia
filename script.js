269c0bdfaa9ee6315ef002512296c1cd6d36922b
JC Botelho <joao@digitalpetals.com.br>
2020-02-28 23:39:56 -0300

    Adicionado textura, mudança do personagem, mudanças de áudio e regra para não nascer inimigo em cima do personagem

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ var memory = {
   persons: [],
   objects: [],
   score: 0,
-  move: 50
+  move: 50,
+  sizePerson: 120,
+  quantityStartEnemy: 20
 };
 
 $('#play').click(function() {
   $('#controller').hide();
   $('#screen').show();
 
-  $('#screen').append('<div class="person"></div>');
-  loadEnemy(20);
+  loadPerson();
+  loadEnemy(memory.quantityStartEnemy);
 
-  setInterval(function() {
-    loadEnemy(5);
-  }, 3000)
+  //track('soundtrack', 'track.mp3');
 });
 
-function track(filename) {
-  var player = document.getElementById('player');
-  player.src = 'assets/sounds/' + filename + '.m4a';
+function track(target, filename) {
+  var player = document.getElementById(target);
+  player.src = 'assets/sounds/' + filename;
   player.play();
 }
 
-$(document).bind({
-  keyup: function(e) {
+$(window).bind({
+  keydown: function(e) {
 
     var bleft = 37;
     var btop = 38;
@@ -32,43 +32,88 @@ $(document).bind({
     var bbottom = 40;
     var bspace = 32;
 
-    var key = e.which;
+    $person = $('.person');
 
-    if (key == bright) {
-      track('pru2');
-      left = parseInt($('.person').css('left'));
+    personleft = parseInt($person.css('left'));
+    persontop = parseInt($person.css('top'));
 
-      $('.person').css({'left': left + memory.move})
-    }
-
-    if (key == bleft) {
-      left = parseInt($('.person').css('left'));
-      if (left > 0) {
-        $('.person').css({'left': left - memory.move});
-      }
-    }
+    switch (e.which) {
+      case bright:
+        $person.css({
+          'left': `${personleft + memory.move}px`,
+          'background-position': '0 0'
+        });
+        break;
     
-    if (key == bbottom) {
-      top = parseInt($('.person').css('top'));
-      $('.person').css({'top': top + memory.move});
-    }
-
-    if (key == bspace) {
-      track('pru');
+      case bleft:
+        if (personleft > 0) {
+          $person.css({
+            'left': `${personleft - memory.move}px`,
+            'background-position': '0 -120px'
+          });
+        }
+        break;
+      
+      case bbottom:
+        $person.css({
+          'top': `${persontop + memory.move}px`,
+          'background-position': '-240px 0'
+        });
+        break;
+      
+      case btop:
+        $person.css({
+          'top': `${personleft - memory.move}px`
+        });
+        break;
+      
+      case bspace:
+        track('effects', 'pru.m4a');
+        break;
     }
   }
 });
 
 function loadEnemy(quantity) {
-  width = parseInt($(document).width()) - 100;
-  height = parseInt($(document).height()) - 100;
+  width = parseInt($(document).width()) - 120;
+  height = parseInt($(document).height()) - 120;
+
+  console.log('>>>', width, height);
 
   for (let i = 0; i < quantity; i++) {
-    $('#screen').append('<div class="enemy" style="left: ' + getRandomArbitrary(width) + 'px; top: ' 
-    + getRandomArbitrary(height) + 'px;"></div>');
+    $enemy = $(`<div></div>`);
+    $enemy.addClass('enemy');
+    $enemy.css({
+      'left': getRandomArbitrary(width),
+      'top': getRandomArbitrary(height)
+    });
+
+    $enemy.appendTo('#screen');
   }
 }
 
+function loadPerson() {
+  $person = $('<div></div>');
+  $person.addClass('person');
+  $person.css({
+    width: `${memory.sizePerson}px`,
+    height: `${memory.sizePerson}px`,
+    top: '0px',
+    left: '0px'
+  });
+  $person.appendTo('#screen');
+}
+
 function getRandomArbitrary(max) {
-  return Math.random() * (max - 0) + 0;
+  $person = $('.person');
+  var x = parseInt($person.css('left')),
+      y = parseInt($person.css('top')),
+      width = parseInt($person.css('width')),
+      height = parseInt($person.css('height')),
+      minx = x + width,
+      miny = y + height;
+
+  console.log(max, x, y);
+
+  return Math.random() * (max - minx) + minx;
 }
